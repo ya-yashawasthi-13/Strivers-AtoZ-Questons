016ac1a84fc1e2159d2ce6caedb02e80f2cc542e
Yashawasthi71 <yashawasthi88@gmail.com>
2025-06-26 17:26:22 +0530

    update

--- Array_sorted_rotated.cpp
+++ Array_sorted_rotated.cpp
@@ -88,3 +88,33 @@ int main()
 
     return 0;
 }*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> arr = {
+        1,
+        2,
+        3,
+        4,
+        5,
+    };
+
+    int n = arr.size();
+
+    int temp = arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        arr[i - 1] = arr[i];
+    }
+
+    arr[n - 1] = temp;
+
+    for (auto i : arr)
+    {
+        cout << i << " ";
+    }
+}
