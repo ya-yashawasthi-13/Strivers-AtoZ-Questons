057f2d154ba739f05eb00603cdf7dcde2d76442b
Yashawasthi71 <yashawasthi88@gmail.com>
2025-07-02 14:36:42 +0530

    update

--- Leader_Array.cpp
+++ Leader_Array.cpp
@@ -2,7 +2,7 @@
 #include<string>
 using namespace std;
 
-int main(){
+/*int main(){
     vector<int> arr = {10,22,12,3,0,6} ;
     vector<int> leaders ;
     int n = arr.size() ;
@@ -23,4 +23,28 @@ int main(){
     }
     cout << endl ;
     return 0 ;
+}*/
+
+
+int main(){
+    vector<int> arr = {10,22,12,3,0,6} ;
+    int n = arr.size() ;
+    int leader = arr[n- 1] ;
+    vector<int> leaders ;
+
+    leaders.push_back(leader) ;
+
+    for(int i = n - 2 ; i >= 0 ; i--){
+        if(arr[i] > leader){
+            leader = arr[i] ;
+            leaders.push_back(leader) ;
+        }
+    }
+    reverse(leaders.begin(), leaders.end()) ;
+    for(auto i : leaders){
+        cout << i << " " ;
+    }
+    cout << endl ;
+    return 0 ;
 }
+
